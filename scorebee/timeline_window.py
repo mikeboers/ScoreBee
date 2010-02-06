6d6aaebbbf8b62791f81e1ff2e41a07560533598
Mike Boers <github@mikeboers.com>
2010-02-06 13:31:23 -0500

    Timeline window based off of QMainWindow.

--- scorebee/timeline_window.py
+++ scorebee/timeline_window.py
@@ -3,12 +3,15 @@ from .qt import *
 from .ui.timeline_window import Ui_timeline_window
 
 
-class TimelineWindow(QtGui.QWidget):
+class TimelineWindow(QtGui.QMainWindow):
 
     def __init__(self, *args):
-        QtGui.QWidget.__init__(self, *args)
+        QtGui.QMainWindow.__init__(self, *args)
         self.ui = Ui_timeline_window()
         self.ui.setupUi(self)
+        
+        # That the stupid status bar.
+        self.setStatusBar(None)
     
     def set_canvas_height(self, height):    
         self.ui.canvas.setMinimumSize(0, height)
