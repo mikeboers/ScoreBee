1d19245f6b111cc74b6f82df3f87ffc8ac8a1e1f
Mike Boers <github@mikeboers.com>
2010-02-06 13:29:53 -0500

    Monkey patching Qt.

--- scorebee/qt.py
+++ scorebee/qt.py
@@ -12,3 +12,37 @@ def debug():
     pyqtRemoveInputHook()
     pdb.set_trace()
     pyqtRestoreInputHook()
+
+
+def patch(class_):
+    def patcher(method):
+        setattr(class_, method.__name__, method)
+    return patcher
+
+
+@patch(QPoint)
+def __iter__(self):
+    return iter((self.x(), self.y()))
+
+
+@patch(QPoint)
+def __getitem__(self, i):
+    if i == 0:
+        return self.x()
+    elif i == 1:
+        return self.y()
+    raise IndexError(i)
+
+  
+@patch(QSize)
+def __iter__(self):
+    return iter((self.width(), self.height()))
+
+
+@patch(QSize)
+def __getitem__(self, i):
+    if i == 0:
+        return self.width()
+    elif i == 1:
+        return self.height()
+    raise IndexError(i)
