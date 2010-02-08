5faf29e13ceec7e17bcc9a4c8b6d0da70fb6e676
Mike Boers <github@mikeboers.com>
2010-02-08 10:43:08 -0500

    Massive changes to the mplayer.

--- scorebee/mplayer.py
+++ scorebee/mplayer.py
@@ -11,99 +11,115 @@ import time
 log = logging.getLogger(__name__)
 
 
-def make_property(name, parser=str, force_get=True, force_set=True, buffer=False):
-
-    get_cmd = 'pausing_keep%s get_property %s' % ('_force' if force_get else '', name)
-    set_cmd = 'pausing_keep%s set_property %s %%s' % ('_force' if force_set else '', name)
-
-    buffer_attr_name = '_' + name + '_buffer'
-
-    res_prefix = 'ANS_%s=' % name
-    res_prefix_len = len(res_prefix)
-
-    @property
-    def prop(self):
-        x = getattr(self, buffer_attr_name, None) if buffer else None
-        if x is not None:
-            return x
-
-        start_time = time.time()
-        self._cmd(get_cmd)
-        while self._readable(self._stdout, 1.0):
-            raw = self._stdout.readline()
-            if not raw.startswith(res_prefix):
-                log.warning('got unrequested %r' % raw.strip())
-                continue
-            try:
-                v = parser(raw[res_prefix_len:])
-                # log.debug('got %s in %.2fms' % (name, 1000 * (time.time() - start_time)))
-                break
-            except:
-                log.exception('error while parsing response')
-                raise
-        else:
-            log.error('request timed out')
-            raise MPlayerComFailure('request timed out')
-
-        if buffer:
-            setattr(self, buffer_attr_name, v)
-        return v
-
-    @prop.setter
-    def prop(self, v):
-        self._cmd(set_cmd % v)
-        setattr(self, buffer_attr_name, v)
-
-    return prop
 
 
-
+class MPlayerEOF(ValueError):
+    pass
 
 class MPlayerDied(ValueError):
     pass
 
 
-class MPlayerComFailure(ValueError):
-    pass
-
 
 class MPlayer(object):
-
-    def __init__(self, src, autoplay=False):
-
-        self.proc = Popen(['mplayer', '-slave', '-quiet', '-framedrop',
-            '-input', 'conf=' + os.path.abspath(__file__ + '/../../settings/mplayer.txt'),
-            src], stdin=PIPE, stderr=None, stdout=PIPE)
-
-        self._stdin = self.proc.stdin
+    
+    # _id_keys is required by the __getattr__ and __setattr__. Unfortunately,
+    # me make requests to thos methods before _id_keys would naturally be set.
+    # I am putting a default here so we don't drop into infinite recursion. 
+    _id_keys = set()
+    
+    def __init__(self, path, conf=None, autoplay=False):
+        
+        self._init_data()
+        self._path = path
+        
+        cmd = ['mplayer', '-slave', '-quiet', '-framedrop', '-identify', ]
+        if conf:
+            cmd.extend(['-input', 'conf=' + conf])
+        cmd.append(path)
+        
+        self.proc = Popen(cmd, stdin=PIPE, stdout=PIPE)
+        
+        self._stdin  = self.proc.stdin
         self._stdout = self.proc.stdout
-        self._stderr = self.proc.stderr
-
-        self._is_paused = False
+        
         if not autoplay:
             self.pause()
-
+            self.time = 0
+        self._read(timeout=0.1)
+    
+    def _init_data(self):
+        
+        # All data read in.
+        self.data = {}
+        
+        # The names of data that is constant.
+        self._id_keys = set()
+        
+        # The current speed.
         self._speed = Fraction(1, 1)
-
-        self._clear_read_buffer(0.1)
+        
+        # It starts automatically playing.
+        self._is_paused = False
 
     def __del__(self):
-        self.stop()
-
-    def _assert_running(self):
+        try:
+            if self.is_running:
+                self._cmd('quit')
+        except:
+            pass
+    
+    is_eof = property(lambda self: 'exit' in self.data)
+    is_running = property(lambda self: self.proc.poll() is None)
+    
+    def _assert_video(self):
         """Make sure the process is still running or throw an error."""
-        if self.proc.poll() is not None:
+        if not self.is_running:
             raise MPlayerDied('mplayer has died')
+        if self.is_eof:
+            raise MPlayerEOF('mplayer hit EOF')
 
-    def _readable(self, pipe, timeout=0):
-        self._assert_running()
-        r, w, x = select([pipe], (), (), timeout)
+    def _readable(self, timeout=0):
+        """Determine if we can read from mplayer without blocking."""
+        if not self.is_running:
+            return False
+        r, w, x = select([self._stdout], (), (), timeout)
         return bool(r)
 
-    def _clear_read_buffer(self, timeout=0):
-        """Clear out the read buffer before we issue a command."""
-        while self._readable(self._stdout, timeout):
-            log.info('cleared %r' % self._stdout.readline().strip())
+    def _read(self, timeout=0, key=None):
+        """Read until we cannot anymore, or find the key we want.
+        
+        The timeout applies for every line read, so we can easily wait many
+        times the supplied timeout.
+        
+        """
+        desired_key = key
+        while self._readable(timeout):
+            key = None
+            line = self._stdout.readline().strip()
+            # log.debug(line)
+            
+            if line.startswith('ANS_'):
+                key, value = line[4:].split('=')
+                try:
+                    value = float(value)
+                except:
+                    pass
+            elif line.startswith('ID_'):
+                key, value = line[3:].split('=')
+                self._id_keys.add(key.lower())
+                try:
+                    value = int(value)
+                except:
+                    try:
+                        value = float(value)
+                    except:
+                        pass
+            if key is not None:
+                key = key.lower()
+                self.data[key] = value
+                if key == desired_key:
+                    return value
 
     def _cmd(self, cmd):
         """Makes a request to mplayer.
@@ -113,72 +129,83 @@ class MPlayer(object):
         """
         self._stdin.write(cmd + '\n')
         self._stdin.flush()
+    
+    def get_property(self, name, timeout=1.0, pausing_keep_force=True):
+        self._assert_video()
+        self._cmd('%s get_property %s' % ('pausing_keep_force' if pausing_keep_force else 'pausing_keep', name))
+        return self._read(timeout=timeout, key=name)
+    
+    def set_property(self, name, value, pausing_keep_force=True):
+        self._assert_video()
+        self._cmd('%s set_property %s %s' % ('pausing_keep_force' if pausing_keep_force else 'pausing_keep', name, value))
 
-    def _cmd_read(self, cmd, timeout=1.0):
-        """Makes a request and waits for a response."""
-        self._clear_read_buffer()
-        self._stdin.write(cmd + '\n')
-        self._stdin.flush()
-        if self._readable(self._stdout, timeout):
-            return self._stdout.readline().strip()
 
-    @property
-    def is_running(self):
-        return self.proc.poll() is None
-
-    @property
-    def is_paused(self):
-        return self._is_paused
-
-    @property
-    def is_playing(self):
-        return not self._is_paused
+    is_paused = property(lambda self: self._is_paused)
+    is_playing = property(lambda self: not self._is_paused)
+    
 
     def pause(self):
+        """Pause if playing."""
         if not self._is_paused:
             self._cmd('pausing_keep_force pause')
             self._is_paused = True
 
     def play(self):
+        """Play if paused."""
         if self._is_paused:
             self._cmd('pause')
             self._is_paused = False
     
+    def toggle_pause(self):
+        """Toggle pause state. Play if paused and pause if playing."""
+        self._cmd('pause')
+        self._is_paused = not self._is_paused
+    
     def step(self):
+        """Pause and step to the next frame."""
         self._is_paused = True
         self._cmd('pausing frame_step')
 
     def stop(self):
-        """Stop playing and kill the process."""
-        if self.is_running:
-            self._cmd('exit')
-            self.proc.kill()
+        """Stop playing."""
+        self._cmd('stop')
 
-    @property
-    def frame(self):
-        return int(math.ceil(self.fps * self.time))
 
+    time = property(lambda self: self.get_property('time_pos'))
+    time = time.setter(lambda self, v: self.set_property('time_pos', v, pausing_keep_force=False))
+    
+    fps = property(lambda self: self.data['video_fps'])
+    
+    frame = property(lambda self: int(self.fps * self.time + 0.5))
     @frame.setter
     def frame(self, value):
         """This is not exact. It will only get close."""
         self.time = float(value) / float(self.fps)
 
-    @property
-    def frame_count(self):
-        return int(self.fps * self.length)
+    frame_count = property(lambda self: int(self.fps * self.length))
 
-    @property
-    def speed(self):
-        return self._speed
-
+    speed = property(lambda self: self._speed)
     @speed.setter
     def speed(self, value):
         self._speed = Fraction(value)
-        self._raw_speed = float(value)
+        self.set_property('speed', float(value))
+    
+    def __getattr__(self, name):
+        if name in self._id_keys:
+            return self.data[name]
+        return object.__getattribute__(self, name)
+    # 
+    # def __setattr__(self, name, value):
+    #     print 'set', repr(name)
+    #     if name in self._id_keys:
+    #         raise ValueError('cannot set property %r' % name)
+    #     
+    #     # This should be a list of properies we will let you set.
+    #     if False: #name in ():
+    #         self.set_property(name[5:] if name.startswith('_raw_') else name, value)
+    #     else:
+    #         object.__setattr__(self, name, value)
 
-    _raw_speed  = make_property('speed', float)
-    fps    = make_property('fps', float, buffer=True)
-    length = make_property('length', float, buffer=True)
 
-    time = make_property('time_pos', float, force_set=False)
-    percent = make_property('percent_pos', float)
+if __name__ == '__main__':
+    mp = MPlayer('/Users/mikeboers/Desktop/example.MOV')
