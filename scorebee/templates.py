5aab9049d41d9b3ad3ddb3b6323e728b78746187
Mike Boers <github@mikeboers.com>
2010-02-09 12:40:25 -0500

    Basic template for tanya.

--- scorebee/templates.py
+++ scorebee/templates.py
@@ -11,6 +11,22 @@ templates = {
         dict(name='Asdf', key='a', group='one'),
         dict(name='Sdfg', key='s', group='two'),
         dict(name='Dfgh', key='d', group='three'),
+    ],
+    
+    'General Flies': [
+        dict(name='approach',                key='p', group=None),
+        dict(name='low-level fencing',       key='w', group=None),
+        dict(name='wing threat',             key='e', group=None),
+        dict(name='high-level fencing',      key='a', group=None),
+        dict(name='chasing',                 key='s', group=None),
+        dict(name='lunging',                 key='d', group=None),
+        dict(name='holding',                 key='a', group=None),
+        dict(name='boxing',                  key='s', group=None),
+        dict(name='tussling',                key='d', group=None),
+        dict(name='walk away',               key='a', group=None),
+        dict(name='defensive wing threat',   key='s', group=None),
+        dict(name='run away / being chased', key='d', group=None),
+        dict(name='fly away',                key='d', group=None),
     ]
 
 }
