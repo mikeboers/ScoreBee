1f331c1ec7c37202a94cad00337ccbcda6b5ccd2
Mike Boers <github@mikeboers.com>
2010-02-09 13:42:44 -0500

    Tanya's template.

--- scorebee/templates.py
+++ scorebee/templates.py
@@ -14,19 +14,26 @@ templates = {
     ],
     
     'General Flies': [
-        dict(name='approach',                key='p', group=None),
-        dict(name='low-level fencing',       key='w', group=None),
-        dict(name='wing threat',             key='e', group=None),
-        dict(name='high-level fencing',      key='a', group=None),
-        dict(name='chasing',                 key='s', group=None),
-        dict(name='lunging',                 key='d', group=None),
-        dict(name='holding',                 key='a', group=None),
-        dict(name='boxing',                  key='s', group=None),
-        dict(name='tussling',                key='d', group=None),
-        dict(name='walk away',               key='a', group=None),
-        dict(name='defensive wing threat',   key='s', group=None),
-        dict(name='run away / being chased', key='d', group=None),
-        dict(name='fly away',                key='d', group=None),
+        dict(name='approach',                key='u', group=None),
+        dict(name='low-level fencing',       key='i', group=None),
+        dict(name='wing threat',             key='o', group=None),
+        dict(name='high-level fencing',      key='h', group=None),
+        dict(name='chasing',                 key='j', group=None),
+        dict(name='lunging',                 key='k', group=None),
+        dict(name='holding',                 key='l', group=None),
+        dict(name='boxing',                  key='b', group=None),
+        dict(name='tussling',                key='n', group=None),
+        dict(name='walk away',               key='m', group=None),
+        
+        dict(name='defensive wing threat',   key='r', group=None),
+        dict(name='run away / being chased', key='e', group=None),
+        dict(name='fly away',                key='w', group=None),
+        
+        dict(name='non-aggressive present',  key='f', group=None),
+        dict(name='non-aggressive absent',   key='d', group=None),
+        dict(name='non-aggressive arrive',   key='s', group=None),
+        dict(name='non-aggressive leave',    key='a', group=None),
+
     ]
 
 }
